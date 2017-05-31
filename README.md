d666a404acf4947f2e3a1cbe6d30848abd79885b
Hampus Reis <crhampus@gmail.com>
2017-05-31 06:49:49 +0200

    Initial commit
    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hampus_API
+
+#HowTo Use
+
+1. Open Terminal/Powershell.
+2. Type "node index.js"
+3. Done.
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const soap = require('soap')
+const bodyParser = require('body-parser')
+
+const STUDENT_SERVICE_URL = 'http://193.10.202.71/UserServiceApplication/UserService.svc?wsdl'
+const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+
+function getClient() {
+    return new Promise((resolve, reject) => {
+        soap.createClient(STUDENT_SERVICE_URL, (err, client) => {
+            if(err) return reject(err)
+            resolve(client)
+        })
+    })
+}
+
+app.get('/api/student', async (req, res) => {
+    let socsecnum = req.query.socsecnum
+    try {
+        let client = await getClient()
+        client.GetUserBySocSecNum({socSecNum: socsecnum}, function(err, result){
+            if(err) return res.status(404).json(err)
+            res.status(200).json(result)
+        })
+    }catch(e) {
+        res.status(404).json(e)
+    }
+})
+
+app.listen(8080)
